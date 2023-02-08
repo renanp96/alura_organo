005245c73ec93751c2d3800f6935abbcd5a152b3
renanp96 <renanpa1996@gmail.com>
2023-02-08 16:38:05 -0300

    fix: prevenindo comportamento padrao do botao submit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,14 +7,19 @@ export const Form = ()=> {
 
     const classes = ['Archer', 'Assassin', 'Avenger', 'Berserker', 'Caster', 'Lancer', 'Rider', 'Ruler', 'Saber']
 
+    const onSave = (event) => {
+        event.preventDefault();
+        console.log("Form submetido");
+    }
+
     return(
         <section className="form">
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para criar o card dos servos</h2>
-                <InputText label="Nome" placeholder="Digite o nome..." />
-                <InputText label="Titulo" placeholder="Digite o titulo..." />
+                <InputText required={true} label="Nome" placeholder="Digite o nome..." />
+                <InputText required={true} label="Titulo" placeholder="Digite o titulo..." />
                 <InputText label="Imagem" placeholder="Digite o endereço da imagem..." />
-                <DropBox label="Classe" itens={classes}/>
+                <DropBox required={true} label="Classe" itens={classes}/>
                 <Button>Criar Card</Button>
             </form>
         </section>
