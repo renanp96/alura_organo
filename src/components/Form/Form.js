68bc87ff09aea7e92f9c5bad02aaa720272669ca
renanp96 <renanpa1996@gmail.com>
2023-02-08 13:58:37 -0300

    feature: implementada a lista suspensa

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,19 @@
+import DropBox from '../DropBox';
 import InputText from '../InputText';
 import './Form.css';
 
 export const Form = ()=> {
+
+    const classes = ['Saber', 'Lancer', 'Archer', 'Rider', 'Assassin', 'Caster', 'Berserker', 'Ruler', 'Avenger'];
+
     return(
         <section className="form">
             <form>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <InputText label="Nome" placeholder="Digite seu nome..." />
-                <InputText label="Cargo" placeholder="Digite seu cargo..." />
+                <h2>Preencha os dados para criar o card dos servos</h2>
+                <InputText label="Nome" placeholder="Digite o nome..." />
+                <InputText label="Titulo" placeholder="Digite o titulo..." />
                 <InputText label="Imagem" placeholder="Digite o endereço da imagem..." />
+                <DropBox label="Classe" itens={classes}/>
             </form>
         </section>
     );
