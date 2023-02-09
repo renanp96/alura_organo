4a9c4f9eb2e505de043ec28248ce263cfcd6c7b0
renanp96 <renanpa1996@gmail.com>
2023-02-09 10:52:56 -0300

    feature: criada estrutura e estilizacao inicial do componente Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import './Card.css';
+
+export const Card = () => {
+    return(
+        <div className='card'>
+            <div className='header'>
+                <img src='' alt=''/>
+            </div>
+            <div className='footer'>
+                <h4>Gilgamesh</h4>
+                <h5>King of Heroes</h5>
+            </div>
+        </div>
+    );
+}
