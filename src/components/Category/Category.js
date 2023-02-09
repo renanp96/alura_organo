cd76dcce2a87c45ca27f6318d51545dc401f39ba
renanp96 <renanpa1996@gmail.com>
2023-02-09 10:52:05 -0300

    feature: testando a implementacao do componente Card

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,12 +1,15 @@
+import { Card } from '../Card/Card';
 import './Category.css';
 
 export const Category = (props) => {
     const secondaryColor = { backgroundColor: props.secondaryColor };
     const primaryColor = { borderColor: props.primaryColor };
 
-    return(
+    return (
         <section className="category" style={secondaryColor}>
             <h3 style={primaryColor}>{props.name}</h3>
+            <Card />
+            <Card />
         </section>
     );
 }
